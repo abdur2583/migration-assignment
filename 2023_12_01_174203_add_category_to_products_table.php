c155a6fdd1c292d9498e3e24ceab738b7a9b09f3
Abdur Rahman <44979323+abdur2583@users.noreply.github.com>
2023-12-01 20:53:10 +0300

    Add files via upload

---
+++ 2023_12_01_174203_add_category_to_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //add a new column called "quantity" to the "products" table
+            $table->after('description', function ($table) {
+                $table->integer('quantity')->nullable();
+            })
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
